47a41edde74fbc526942f858fc39bbdca79f49c3
NilsWestgardh <72079716+NilsWestgardh@users.noreply.github.com>
2023-09-27 14:11:33 +0000

    Reverted back to functional version without Supabase

--- public/index.js
+++ public/index.js
@@ -1,31 +1,8 @@
-// Initialize Supabase
-const supabaseUrl = 'https://ysrdbmhovvypyaqxylqj.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzcmRibWhvdnZ5cHlhcXh5bHFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc3ODM4MTQsImV4cCI6MjAwMzM1OTgxNH0.ZDgPFs-T8SwuCih5d_7hRdG7wqDvHzUYqx5DGLNhHT8';
-const supabase = window.createClient(supabaseUrl, supabaseKey);
+console.log("Script loaded");
 
-// Google Sign-In
-async function signInWithGoogle() {
-  const { user, error } = await supabase.auth.signIn({
-    provider: 'google'
-  });
-  // Handle user and error accordingly
-}
-
-// Create Card in Supabase
-async function createCardInSupabase(cardName, cardData) {
-  console.log("createCardInSupabase function is running");
-  const { data, error } = await supabase
-    .from('Cards')
-    .insert([
-      { card_name: cardName, card_data: cardData },
-    ]);
-
-  if (data) {
-    console.log("Card successfully inserted:", data);
-  } else if (error) {
-    console.error("Error inserting card:", error);
-  }
-}
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM fully loaded and parsed");
+});
 
 // Function to map shorthand energy to actual file name
 function mapEnergyToFile(energy) {
@@ -35,7 +12,6 @@ function mapEnergyToFile(energy) {
     'P': 'Purple',
     'R': 'Red',
     'G': 'Green',
-    '0': 'Number=Zero',
     '1': 'Number=1',
     '2': 'Number=2',
     '3': 'Number=3',
@@ -49,7 +25,6 @@ function mapEnergyToFile(energy) {
 
 // Function to update the card
 function updateCard(cardData) {
-  console.log("updateCard function is running");
   // Update card color background
   const cardBackground = document.querySelector('.card-background');
   cardBackground.className = `card-background card-bg-${cardData["Card Color"].toLowerCase()}`;
@@ -63,14 +38,17 @@ function updateCard(cardData) {
   cardCost.innerHTML = '';  // Clear existing cost icons
   const energyCost = cardData["Card Energy Cost"];
   energyCost.match(/\{[0-9A-Z]+\}/g).forEach(icon => {
-    const img = document.createElement('img');
-    img.className = 'img-fluid card-cost-icon';
     const mappedEnergy = mapEnergyToFile(icon.replace(/[{}]/g, ''));
-    img.src = `images/icons/Energy=${mappedEnergy}.png`;
-    console.log("Image src set to:", img.src);
-    cardCost.appendChild(img);
+    
+    // Skip adding the Number=0.png image
+    if (mappedEnergy !== 'Number=0') {
+      const img = document.createElement('img');
+      img.className = 'img-fluid card-cost-icon';
+      img.src = `images/icons/Energy=${mappedEnergy}.png`;
+      cardCost.appendChild(img);
+    }
   });
-
+  
   // Update card type
   const cardType = document.querySelector('.card-type');
   cardType.textContent = cardData["Card Type"];
@@ -80,9 +58,9 @@ function updateCard(cardData) {
   cardGrade.src = `images/icons/Style=Medal, Quality=${cardData["Card Grade"]}.png`;
 
   // Update card text
-const cardTextArray = cardData["Card Text"];
-const cardTextBox = document.querySelector('.frame-text-box');
-cardTextBox.innerHTML = ''; // Clear existing text
+  const cardTextArray = cardData["Card Text"];
+  const cardTextBox = document.querySelector('.frame-text-box');
+  cardTextBox.innerHTML = ''; // Clear existing text
 
   // Loop through the generated text array and append to the card
   cardTextArray.forEach((text, index) => {
@@ -103,10 +81,10 @@ cardTextBox.innerHTML = ''; // Clear existing text
     cardTextBox.appendChild(flavorTextParagraph);
   }
 
-  console.log(cardData);
   // Update card stats
   const cardStats = document.querySelector('.frame-stats p');
-  if (cardData["Card Type"] === "Entity" || cardData["Card Type"] === "Voidwarper") {
+  console.log("Card Type:", cardData["Card Type"]);
+  if (cardData["Card Type"].startsWith("Entity") || cardData["Card Type"].startsWith("Voidwarper")) {
     cardStats.textContent = cardData["Card Stats"];
     cardStats.parentElement.style.display = "block";  // Make sure the stats box is visible
   } else {
@@ -121,7 +99,7 @@ cardTextBox.innerHTML = ''; // Clear existing text
 
 // Function to generate a card
 async function generateCard() {
-  console.log("generateCard function is running");
+  console.log("Generating card...");
   // Get the prompt from the input field
   const prompt = document.getElementById("openai-prompt").value;
 
@@ -136,33 +114,13 @@ async function generateCard() {
 
   // Parse the JSON response
   const data = await response.json();
-  console.log(data);
 
   // Update the card on the webpage
   updateCard(data);
+  console.log("Card generated and updated.");
 }
 
 // Attach the function to the button
-document.querySelector("button").addEventListener("click", async () => {
-  console.log("Button clicked!");
+document.querySelector(".custom-btn").addEventListener("click", async () => {
   await generateCard();
-
-  // Populate cardData from DOM elements
-  const cardName = document.querySelector('.card-name').textContent;
-  const cardType = document.querySelector('.card-type').textContent;
-  const cardGrade = document.querySelector('.card-set-icon').src.split("=").pop().split(".")[0];
-  const cardStats = document.querySelector('.frame-stats p').textContent;
-  const cardArt = document.querySelector('.frame-art img').src;
-
-  // Create an object to hold the card data
-  const cardData = {
-    cardName,
-    cardType,
-    cardGrade,
-    cardStats,
-    cardArt
-  };
-
-  // Save it to Supabase
-  await createCardInSupabase(cardName, cardData);
 });
--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 // Importing the required modules
 const express = require('express');
 const cors = require('cors');
-const { createClient } = require('@supabase/supabase-js');
 
 // Dynamic import for node-fetch
 let fetch;
 import('node-fetch').then(module => {
   fetch = module.default;
 });
-
-console.log("Debugging OpenAI API initialization:");
-console.log("API Key:", process.env['OPENAI_API']);
-
-// Initialize the Supabase client
-const supabase = createClient("https://ysrdbmhovvypyaqxylqj.supabase.co", process.env['SUPABASE_API']);
 
 // Initialize the Express app
 const app = express();
@@ -35,20 +28,21 @@ app.get('/', (req, res) => {
 // Function to convert generated text to card data
 function convertToCardData(generatedText) {
   try {
+    console.log("Generated Text:", generatedText);
     const cardData = JSON.parse(generatedText);
     cardData["Card Text"] = Array.isArray(cardData["Card Text"]) ? cardData["Card Text"] : [cardData["Card Text"]];
     return cardData;
   } catch (error) {
     console.error("Error parsing generated text:", error);
-    // Fallback to a sample cardData object if parsing fails
     return {
       "Card Color": "Blue",
       "Card Name": "Cybernetic Dragon",
       "Card Energy Cost": "{2}{Y}",
-      "Card Type": "Entity",
+      "Card Type": "Entity — Dragon Mech",
       "Card Grade": "Rare",
       "Card Text": ["Deploy — Do something", "Flight"],
-      "Card Stats": "4/4"
+      "Card Stats": "4/4",
+      "Card Art": "A dragon mech with a cybernetic body and wings."
     };
   }
 }
@@ -59,22 +53,30 @@ app.post('/generate-card', async (req, res) => {
   const predefinedPrompt = `
 Role: You're an expert TCG game designer.
 Objective: Generate an original Nexus: Voidwarpers card that strictly aligns with the game's existing rules and mechanics.
-Inspiration/reference: Existing Nexus: Voidwarper cards.
-Game rules: https://nexus-voidwarpers-1.nilswestgardh.repl.co/rules.html
+Game rules: https://nexus-voidwarpers.nilswestgardh.repl.co/rules.html
+Inspiration/reference: Existing Nexus: Voidwarper cards: https://nexus-voidwarpers.nilswestgardh.repl.co/cards.html
 Existing Nexus cards: https://nexus-voidwarpers-1.nilswestgardh.repl.co/cards.html
-Avoid: Borrowing mechanics or syntax from other TCGs like MTG and Hearthstone. Do not invent new keywords or mechanics not found in Nexus: Voidwarpers.
+Avoid: Do NOT use mechanics or syntax from other TCGs like MTG and Hearthstone. Do NOT invent new keywords or mechanics NOT found in Nexus: Voidwarpers game rules. Do NOT include an explanation.
+Format: Return a JSON object with the following keys:
 
-Return the card details as a JSON object with the following keys:
+      "Card Color": "[Color]",
+      "Card Name": "[Name]",
+      "Card Energy Cost": "[Cost]",
+      "Card Type": "[Type — Subtype]",
+      "Card Grade": "[Grade]",
+      "Card Text": ["[Ability/effect]", "[Ability/effect]", "[Ability/effect]"],
+      "Card Stats": "[Stats]",
+      "Card Art": "[Art description / DALL-E prompt]"
 
-"Card Color": Based on game rules and existing Nexus cards. (e.g., "Blue", "Red")
-"Card Name": Dynamic, inspired by the game's lore. (e.g., "Cybernetic Dragon")
-"Card Energy Cost": Scaled to the card's power level. (e.g., "{2}{Y}")
-"Card Type": Based on game rules and user input. Include subtype(s) for Entities. (e.g., "Entity — Entity Type(s)")
-"Card Grade": Based on the card's power level. (Common > Uncommon > Rare > Prime)
-"Card Text": [Ability/Effect 1], [Ability/Effect 2], [Ability/Effect 3], etc. Typically 1-3 abilities or effects, though up to 5-6 allowed. Typically higher grade = more abilities/effects. Stick to existing game rules and mechanics. (e.g., "Deploy — Return up to one target machine card from your cache to your hand. (This triggers whenever this card enters the battlezone.)")
-"Card Stats": Include only for Entity or Voidwarper types. (e.g., "4/4" for Entity, "5" for Voidwarper)
-"Card Art": Write a DALL-E prompt for the artwork with this formatting: "Artstation style, concept art, digital painting, trading card game style, sci-fi, fantasy. [Short, explicit instruction for subject matter]. Dramatic mood, [Short, explicit instruction for composition]."
-Prioritize in order if tokens are limited: "Card Text", "Card Type", "Card Name"
+      Example:
+      "Card Color": "Blue",
+      "Card Name": "Cybernetic Dragon",
+      "Card Energy Cost": "{2}{Y}",
+      "Card Type": "Entity — Dragon Mech",
+      "Card Grade": "Rare",
+      "Card Text": ["Deploy — Do something", "Flight"],
+      "Card Stats": "4/4",
+      "Card Art": "A dragon mech with a cybernetic body and wings."
   `;
 
   const finalPrompt = `${predefinedPrompt}\nUser Input: ${userPrompt}`;
@@ -82,6 +84,7 @@ Prioritize in order if tokens are limited: "Card Text", "Card Type", "Card Name"
   const apiKey = process.env['OPENAI_API'];
 
   try {
+    // Fetch card data from ChatGPT
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: {
@@ -105,40 +108,38 @@ Prioritize in order if tokens are limited: "Card Text", "Card Type", "Card Name"
 
     const data = await response.json();
     console.log("OpenAI API Response:", data);
-
     const generatedText = data.choices[0].message.content.trim();
-    console.log(generatedText);
-    const cardData = convertToCardData(generatedText);  // Convert the generated text to card data
+    const cardData = convertToCardData(generatedText);
 
-    // Construct a DALL-E prompt based on cardData
-    const dallEPrompt = `Concept art, digital painting, sci-fi, fantasy. ${cardData["Card Name"]}. ${cardData["Card Type"]}`;
+    // Assuming cardData["Card Art"] contains the art description
+    console.log(cardData);
+    const artDescription = cardData["Card Art"];
+
+    // Your custom instructions
+    const customInstructions = "Artstation style, trading card game style, sci-fi, fantasy, digital painting. ";
     
-    try {
-      // Make an API call to DALL-E
-      const dallEResponse = await fetch('https://api.openai.com/v1/images/generations', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`
-        },
-        body: JSON.stringify({
-          model: 'image-alpha-001',
-          prompt: dallEPrompt,
-          n: 1  // Number of images to generate
-        })
-      });
-    
-      const dallEData = await dallEResponse.json();
-      console.log("DALL-E API Response:", dallEData);
-    
-      // Attach the generated artwork URL to cardData
-      cardData["Card Art"] = dallEData.data[0].url;
-    
-    } catch (error) {
-      console.error("DALL-E API Error:", error);
-    }
-    
-    res.json(cardData);  // Send the card data back
+    // Fetch image from DALL-E
+    const imageResponse = await fetch('https://api.openai.com/v1/images/generations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        model: 'image-alpha-001',
+        prompt: `${artDescription}. ${customInstructions}`,
+        n: 1  // Number of images you want to generate
+      })
+    });
+
+    const imageData = await imageResponse.json();
+    console.log(imageData);
+    const imageUrl = imageData.data[0].url;  // Assuming the URL of the image is stored here
+
+    // Add the image URL to cardData
+    cardData["Card Art"] = imageUrl;
+
+    res.json(cardData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
